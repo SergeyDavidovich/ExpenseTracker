dcd94ea0158e91c8e3a431499d8d7b2a7e035548
Sergiusz Kriwobokow <writesd@hotmail.com>
2021-10-07 17:22:00 +0200

    Add project files.

---
+++ ExpenseTracker/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Blazor;
+
+namespace ExpenseTracker
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            //Register Syncfusion license 
+            Syncfusion.Licensing.SyncfusionLicenseProvider
+                .RegisterLicense("NTEwNjU1QDMxMzkyZTMzMmUzMEY4TVVBZm5UK25DN01YRG5md3RKSkRDdEZnVDNub1FUemJPQThpU3ZGOVU9");
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddSyncfusionBlazor();
+            await builder.Build().RunAsync();
+        }
+    }
+}
